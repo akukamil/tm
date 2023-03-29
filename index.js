8557841893cddc235045045cc4e8ec2b931f433a
akukamil <chamilioner_kid@mail.ru>
2023-03-29 22:04:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ var prog = {
 		document.getElementById('bricks').style.display = 'none';
 		
 		prog.docClient.query({TableName: "dng7",	KeyConditionExpression: "m_key = :m_key and t_stamp>=:ts",
-		ProjectionExpression: "t_stamp, p_1, p_2, p_3",	ExpressionAttributeValues: { ":m_key": "ASFALT",":ts":start_ts}}, function(err,data){prog.render_kirp_chart(data,"asfalt","Потребление газа (Асфальтовый завод)")});
+		ProjectionExpression: "t_stamp, p_1, p_2, p_3",	ExpressionAttributeValues: { ":m_key": "ASPHALT",":ts":start_ts}}, function(err,data){prog.render_kirp_chart(data,"asfalt","Потребление газа (Асфальтовый завод)")});
 
 	},
 	
