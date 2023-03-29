f7fd2834793ab8d2fa701c7531665452d3668210
akukamil <chamilioner_kid@mail.ru>
2023-03-29 21:57:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -44,6 +44,11 @@ var prog = {
 			return;			
 		}
 		
+		if (local_uid === 'asf') {
+			this.load_data_asf();			
+			return;			
+		}
+		
 		if (local_uid === 'test') {
 			this.load_data_test();			
 			return;			
@@ -141,6 +146,25 @@ var prog = {
 		
 		prog.docClient.query({TableName: "dng7",	KeyConditionExpression: "m_key = :m_key and t_stamp>=:ts",
 		ProjectionExpression: "t_stamp, p_1, p_2, p_3",	ExpressionAttributeValues: { ":m_key": "BRICKS",":ts":start_ts}}, function(err,data){prog.render_brik_chart(data,"bricks","Потребление газа (завод Брикс)")});
+
+	},
+	
+	load_data_asf : function()	{   
+
+		var start_ts=Math.floor(Date.now() / 1000)-40*86400;
+		
+		document.getElementById('sf0').style.display = 'none';
+		document.getElementById('sf1').style.display = 'none';
+		document.getElementById('sf2').style.display = 'none';
+		document.getElementById('sf3').style.display = 'none';
+		document.getElementById('alliance').style.display = 'none';
+		document.getElementById('zarya').style.display = 'none';
+		document.getElementById('sabur').style.display = 'none';
+		document.getElementById('kasp').style.display = 'none';
+		document.getElementById('bricks').style.display = 'none';
+		
+		prog.docClient.query({TableName: "dng7",	KeyConditionExpression: "m_key = :m_key and t_stamp>=:ts",
+		ProjectionExpression: "t_stamp, p_1, p_2, p_3",	ExpressionAttributeValues: { ":m_key": "ASFALT",":ts":start_ts}}, function(err,data){prog.render_kirp_chart(data,"asfalt","Потребление газа (Асфальтовый завод)")});
 
 	},
 	
