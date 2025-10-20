0cd93cd99d273d61ca45fdb1f051b854427fe817
akukamil <chamilioner_kid@mail.ru>
2025-10-20 15:22:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -131,15 +131,7 @@ prog = {
 			ProjectionExpression: "t_stamp, p_1, p_2, p_3, p_4",	ExpressionAttributeValues: { ":m_key": "OSMANOV",":ts":start_ts}}, function(err,data){res(data)})	
 		})
 		prog.render_kirp_chart2(data,"osmanov","Потребление газа (завод ИП ОСМАНОВ)")		
-				
-		/*
-		data=await new Promise(res=>{			
-			prog.docClient.query({TableName: "dng7",	KeyConditionExpression: "m_key = :m_key and t_stamp>=:ts",
-			ProjectionExpression: "t_stamp, p_1, p_2, p_3, p_4",	ExpressionAttributeValues: { ":m_key": "VOSTOK",":ts":start_ts}}, function(err,data){res(data)})	
-		})
-		prog.render_kirp_chart(data,"vostok","Потребление газа (завод ВОСТОК)")		*/
-
-				
+								
 		data=await new Promise(res=>{			
 			prog.docClient.query({TableName: "dng7",	KeyConditionExpression: "m_key = :m_key and t_stamp>=:ts",
 			ProjectionExpression: "t_stamp, p_1, p_2, p_3, p_4",	ExpressionAttributeValues: { ":m_key": "ASPHALT",":ts":start_ts}}, function(err,data){res(data)})	
@@ -193,7 +185,7 @@ prog = {
 		document.getElementById('osmanov').style.display = 'none';
 		
 		prog.docClient.query({TableName: "dng7",	KeyConditionExpression: "m_key = :m_key and t_stamp>=:ts",
-		ProjectionExpression: "t_stamp, p_1, p_2, p_3",	ExpressionAttributeValues: { ":m_key": "ASPHALT",":ts":start_ts}}, function(err,data){prog.render_kirp_chart(data,"asfalt","Потребление газа (Асфальтовый завод)")});
+		ProjectionExpression: "t_stamp, p_1, p_2, p_3",	ExpressionAttributeValues: { ":m_key": "ASPHALT",":ts":start_ts}}, function(err,data){prog.render_kirp_chart2(data,"asfalt","Потребление газа (Асфальтовый завод)")});
 
 	},
 	
